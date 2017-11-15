ba33c6740a54b093a6babafe19b2c167192594d2
Adam Fetters <31798262+adamcodesjava@users.noreply.github.com>
2017-11-15 17:11:01 -0500

    Tree.js seems complete, but tests won't run do to other files

--- src/tree.js
+++ src/tree.js
@@ -7,14 +7,22 @@ class Tree {
   }
   // Adds a new Tree node with the input value to the current Tree node 
   addChild(value) {
-
+    const newNode = new Tree(value);
+    this.children.push(newNode);
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
   contains(value) {
-
+    const firstNode = this.children[0];
+    const checkNode = (currentNode) => {
+      if (currentNode.value === value) return true;
+     currentNode = firstNode.children[0];
+    return checkNode(currentNode);
+    }
+    return checkNode(firstNode);
+    } 
   }
-}
+
 
 module.exports = Tree;
