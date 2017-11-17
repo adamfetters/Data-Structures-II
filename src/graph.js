9e5140cdbdc3879e6159925d6884a6cf337b95f2
Adam Fetters <31798262+adamcodesjava@users.noreply.github.com>
2017-11-17 11:48:23 -0500

    5 of 7 Tests Passing on graph.js

--- src/graph.js
+++ src/graph.js
@@ -40,31 +40,68 @@ class Graph {
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
-
+    const newNode = new GraphNode({ value, edges });
+    if (edges.length > 0) {
+      edges.forEach((element) => {
+        this.addEdge(newNode, element);
+      });
+    }
+    if (this.vertices.length === 1) {
+      const prevNode = this.vertices[0];
+      this.addEdge(newNode, prevNode);
+    } 
+    this.vertices.push(newNode);
+    
+    return newNode;
   }
   // Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-
+    if (this.vertices.length > 0) {
+      for (let i = 0; i < this.vertices.length; i++) {
+        if (this.vertices[i].value === value) return true;
+      }
+    }
+    return false;
   }
+  
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
-
+    for (let i = 0; i < this.vertices.length; i++) {
+      if (this.vertices[i].value === value) {
+        this.vertices.splice(i, 1);
+      }
+    }
   }
+  
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
   // If only one vertex references the other but not vice versa, should not return true
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
-
+    const random = this.vertices;
+    const fromValue = fromVertex.value;
+    const toValue = toVertex.value;
+    const toVArr = toVertex.edges;
+    const fromVArr = fromVertex.edges;
+    let count = 0;
+    for (let i = 0; i < toVArr.length; i++) {
+      if (toVArr[i].value === fromValue) count++;
+    }
+    for (let i = 0; i < fromVArr.length; i++) {
+      if (fromVArr[i].value === toValue) count++;
+    }
+    if (count >= 2) return true;
+    return false;
   }
   // Adds an edge between the two given vertices if no edge already exists between them
   // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
-
+    fromVertex.pushToEdges(toVertex);
+    toVertex.pushToEdges(fromVertex);
   }
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
